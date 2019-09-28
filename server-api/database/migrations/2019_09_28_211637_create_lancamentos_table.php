8e11001366b099d3c714e2cef28b9fe9119ea12b
Rodrigo Pellegrini <rpellegrini@gatefy.com>
2019-09-28 18:43:16 -0300

    First commit

---
+++ server-api/database/migrations/2019_09_28_211637_create_lancamentos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLancamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lancamentos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('tipo');
+            $table->string('descricao');
+            $table->decimal('valor', 8, 2);
+            $table->boolean('status_pagamento')->default(0)
+                  ->comment('0 => Em aberto, 1 => Realizado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lancamentos');
+    }
+}
